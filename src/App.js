cf8080f59cabb83a5d0f3903fe41efbefbc078b2
Naveen9712 <kandakurthinaveen97@gmail.com>
2024-07-18 14:16:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
     const App = () => {
         return (
             <div className="App">
-                <h1>Create User</h1>
                 <UserForm />
             </div>
         );
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,44 +1,66 @@
-import React, { useState } from 'react';
-    import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import "./UserForm.css";
 
-    const UserForm = () => {
-        const [formData, setFormData] = useState({
-            name: '',
-            email: '',
-            password: '',
-        });
+const UserForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-        const handleChange = (e) => {
-            setFormData({ ...formData, [e.target.name]: e.target.value });
-        };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-            try {
-                const response = await axios.post('http://localhost:5000/api/users', formData);
-                console.log('User created:', response.data);
-            } catch (error) {
-                console.error('There was an error creating the user!', error);
-            }
-        };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/users",
+        formData
+      );
+      console.log("User created:", response.data);
+    } catch (error) {
+      console.error("There was an error creating the user!", error);
+    }
+  };
 
-        return (
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name:</label>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} />
-                </div>
-                <button type="submit">Create User</button>
-            </form>
-        );
-    };
+  return (
+    <>
+    <h1 className="form-heading">Create User</h1>
+    <form className="user-form" onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Create User</button>
+    </form>
+    </>
+  );
+};
 
-    export default UserForm;
+export default UserForm;
